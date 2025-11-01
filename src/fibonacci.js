b81bbc3f5c2f7a0b82947ba2bbaf517f31b06ac7
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-01 10:52:06 -0700

    Refactor: simplify solution to iterative fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -4,8 +4,8 @@
  */
 
 /**
- * 
- * @param {Integer} n 
+ * Obtain the first n fibonacci numbers iteratively
+ * @param {Integer} n greater than or equal to 0 
  * @returns {Array} containing the first n fibonacci numbers 
  */
 function fibs(n) {
@@ -20,19 +20,11 @@ function fibs(n) {
     arr.push(f_0);
     return arr;
   }
-  if (n === 2) {
-    arr.push(f_0, f_1);
-    return arr;
-  }
 
   arr.push(f_0, f_1);
-  let f_iLess1 = f_1;
-  let f_iLess2 = f_0;
   for (let i = 2; i < n; i++) {
-    const f_i = f_iLess1 + f_iLess2;
+    const f_i = arr[i-1] + arr[i-2];
     arr.push(f_i);
-    f_iLess2 = f_iLess1;
-    f_iLess1 = f_i;
   }
 
   return arr;
